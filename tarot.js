85c9ffcc2263c18c38398b6e2afc7ad86321324e
Mathew Kane <berkough@gmail.com>
2022-11-05 23:28:13 -0700

    minor re-write

--- tarot.js
+++ tarot.js
@@ -1,11 +1,12 @@
 /*
-A basic library for creating a tarot deck and doing various things to it like shuffling or 
-pulling cards in certain orders or quanitites to be placed in spreads. Eventually I would
-also like to incorporate some sort of wiki-like functionality or allow the cards pulled in 
-spreads to be hovered over and linked to their relevant passages in A.E. Waite's Illustrated 
-Key of the Tarot.
+* A basic library for creating a tarot deck and doing various things to it like shuffling or 
+* pulling cards in certain orders or quanitites to be placed in spreads. Eventually I would
+* also like to incorporate some sort of wiki-like functionality or allow the cards pulled in 
+* spreads to be hovered over and linked to their relevant passages in A.E. Waite's Illustrated 
+* Key of the Tarot.
 */
-let majorArcana = [{Value:'0',Trump:'Fool'},{Value:'I',Trump:'Magician'},
+
+const majorArcana = [{Value:'0',Trump:'Fool'},{Value:'I',Trump:'Magician'},
     {Value:'II',Trump:'High Priestess'},{Value:'III',Trump:'Empress'},{Value:'IV',Trump:'Emperor'},
     {Value:'V',Trump:'Heirophant'},{Value:'VI',Trump:'Lovers'},{Value:'VII',Trump:'Chariot'},
     {Value:'VIII',Trump:'Strength'},{Value:'IX',Trump:'Hermit'},{Value:'X',Trump:'Wheel of Fortune'},
@@ -13,48 +14,95 @@ let majorArcana = [{Value:'0',Trump:'Fool'},{Value:'I',Trump:'Magician'},
     {Value:'XIV',Trump:'Temperance'},{Value:'XV',Trump:'Devil'},{Value:'XVI',Trump:'Tower'},
     {Value:'XVII',Trump:'Star'},{Value:'XVIII',Trump:'Moon'},{Value:'XIX',Trump:'Sun'},
     {Value:'XX',Trump:'Judgement'},{Value:'XXI',Trump:'Universe'}];
-let suits = ['cups','swords','wands','pentacles'];
-let minorValues = ['Ace','II','III','IV','V','VI','VII','VIII','IX','X',
+
+const minorValues = ['Ace','II','III','IV','V','VI','VII','VIII','IX','X',
     'Page','Knight','Queen','King'];
 
-/** This function creates a deck from the above variables. */
-function getDeck(){
-    let deck = [];
-    for (trump of majorArcana){
-        deck.push(trump);
+const suits = ['Cups','Swords','Wands','Pentacles'];
+
+/*
+* The deck constant is a bit like a class... But doesn't have anything that defines it
+* other than the fact that it stores some functions. The "deck" itself doesn't have a 
+* constructor because it will be used by other variables depending on the use. Might convert
+* to a class later...
+*/
+const DECK = {
+    getDeck(){
+        let _deck = [];
+        for (trump of majorArcana){
+            _deck.push(trump);
+        }
+        for (let i = 0; i < suits.length; i++){
+            for (let x = 0; x < minorValues.length; x++){
+                let minorArcana = {
+                    Value: minorValues[x],
+                    Suit: suits[i]
+                };
+                _deck.push(minorArcana);
+            }
+        }
+        return _deck;
+    },
+    shuffle(_deck){
+        for (i = 0; i < (Math.floor(Math.random()*10000)); i++){
+            let one = Math.floor(Math.random()*_deck.length);
+            let two = Math.floor(Math.random()*_deck.length);
+            let three = _deck[one];
+            _deck[one] = _deck[two];
+            _deck[two] = three;
+        }
+    },
+    splitDeck(_deck){
+
     }
-    for (let i = 0; i < suits.length; i++){
-        for (let x = 0; x < minorValues.length; x++){
-            let minorArcana = {
-                Value: minorValues[x],
-                Suit: suits[i]
-            };
-            deck.push(minorArcana);
+}
+
+let newDeck = DECK.getDeck();
+console.log(newDeck[60].Suit);
+
+DECK.shuffle(newDeck);
+console.log(newDeck[10]);
+
+/*
+ * Implementing the game of War using a tarot deck. This is an exercise  in manipulating the deck.
+ */
+
+class Player {
+    constructor(){
+        this.winner = null;
+        this.points;
+    }
+    flipACard(pile){
+        pile.pop();
+    }
+    takeCards(){
+
+    }
+}
+
+const playerOne = new Player;
+const playerTwo = new Player;
+
+class Game {
+    constructor(){
+        this.gameState;
+    }
+    start(){
+        this.gameState = 1;
+        let deck = DECK.getDeck();
+        DECK.shuffle(deck);
+        DECK.splitDeck(deck);
+        this.turn();
+        if(this.gameState = 3){
+
         }
     }
-    return deck;
-}
-let newDeck = getDeck();
-
-/** This function "shuffles" the deck. */
-function shuffle(deck){
-    for (i = 0; i < (Math.floor(Math.random()*10000)); i++){
-        let one = Math.floor(Math.random()*deck.length);
-        let two = Math.floor(Math.random()*deck.length);
-        let three = deck[one];
-        deck[one] = deck[two];
-        deck[two] = three;
+    turn(){
+        if(playerOne.pile  ){
+            
+        } else if (playerTwo.winner = true){
+            
+        }
     }
-    return;
 }
-shuffle(newDeck);
-console.log(newDeck);
 
-let spreadNum = [3, 5, 12];
-let spread =  {Value: 3, Name:'Three Card', Value: 5, Name:'Cross', };
-
-/** This function will take parameters needs for certain spreads.*/
-function createSpread(spreadNum,){
-    // May want a function that calls the properties of a specific spread.
-    // Should take a number of cards used and other attributes of the spread.
-}
