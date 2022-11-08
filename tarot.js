6395f2b45d5fcf7233fe3cbd5e3842d1e59243e7
Mathew Kane <berkough@gmail.com>
2022-11-07 21:41:05 -0800

    tweaks

--- tarot.js
+++ tarot.js
@@ -6,98 +6,107 @@
 * Key of the Tarot.
 */
 
-const majorArcana = [{Value:'0',Trump:'Fool'},{Value:'I',Trump:'Magician'},
-    {Value:'II',Trump:'High Priestess'},{Value:'III',Trump:'Empress'},{Value:'IV',Trump:'Emperor'},
-    {Value:'V',Trump:'Heirophant'},{Value:'VI',Trump:'Lovers'},{Value:'VII',Trump:'Chariot'},
-    {Value:'VIII',Trump:'Strength'},{Value:'IX',Trump:'Hermit'},{Value:'X',Trump:'Wheel of Fortune'},
-    {Value:'XI',Trump:'Justice'},{Value:'XII',Trump:'Hanged Man'},{Value:'XIII',Trump:'Death'},
-    {Value:'XIV',Trump:'Temperance'},{Value:'XV',Trump:'Devil'},{Value:'XVI',Trump:'Tower'},
-    {Value:'XVII',Trump:'Star'},{Value:'XVIII',Trump:'Moon'},{Value:'XIX',Trump:'Sun'},
-    {Value:'XX',Trump:'Judgement'},{Value:'XXI',Trump:'Universe'}];
-
-const minorValues = ['Ace','II','III','IV','V','VI','VII','VIII','IX','X',
-    'Page','Knight','Queen','King'];
-
-const suits = ['Cups','Swords','Wands','Pentacles'];
-
-/*
-* The deck constant is a bit like a class... But doesn't have anything that defines it
-* other than the fact that it stores some functions. The "deck" itself doesn't have a 
-* constructor because it will be used by other variables depending on the use. Might convert
-* to a class later...
-*/
-const DECK = {
+class DECK {
+    constructor(){
+        this._deck = [];
+        
+        this.majorArcana = [{Value:'0',Trump:'Fool'},{Value:'I',Trump:'Magician'},
+        {Value:'II',Trump:'High Priestess'},{Value:'III',Trump:'Empress'},{Value:'IV',Trump:'Emperor'},
+        {Value:'V',Trump:'Heirophant'},{Value:'VI',Trump:'Lovers'},{Value:'VII',Trump:'Chariot'},
+        {Value:'VIII',Trump:'Strength'},{Value:'IX',Trump:'Hermit'},{Value:'X',Trump:'Wheel of Fortune'},
+        {Value:'XI',Trump:'Justice'},{Value:'XII',Trump:'Hanged Man'},{Value:'XIII',Trump:'Death'},
+        {Value:'XIV',Trump:'Temperance'},{Value:'XV',Trump:'Devil'},{Value:'XVI',Trump:'Tower'},
+        {Value:'XVII',Trump:'Star'},{Value:'XVIII',Trump:'Moon'},{Value:'XIX',Trump:'Sun'},
+        {Value:'XX',Trump:'Judgement'},{Value:'XXI',Trump:'Universe'}];
+        
+        this.minorValues = ['Ace','II','III','IV','V','VI','VII','VIII','IX','X',
+        'Page','Knight','Queen','King'];
+        
+        this.suits = ['Cups','Swords','Wands','Pentacles'];
+    }
     getDeck(){
-        let _deck = [];
-        for (trump of majorArcana){
-            _deck.push(trump);
+        for (let i = 0; i < this.majorArcana.length; i++){
+            this._deck.push(this.majorArcana[i]);
         }
-        for (let i = 0; i < suits.length; i++){
-            for (let x = 0; x < minorValues.length; x++){
+        for (let i = 0; i < this.suits.length; i++){
+            for (let x = 0; x < this.minorValues.length; x++){
                 let minorArcana = {
-                    Value: minorValues[x],
-                    Suit: suits[i]
+                    Value: this.minorValues[x],
+                    Suit: this.suits[i]
                 };
-                _deck.push(minorArcana);
+                this._deck.push(minorArcana);
             }
         }
-        return _deck;
-    },
-    shuffle(_deck){
-        for (i = 0; i < (Math.floor(Math.random()*10000)); i++){
-            let one = Math.floor(Math.random()*_deck.length);
-            let two = Math.floor(Math.random()*_deck.length);
-            let three = _deck[one];
-            _deck[one] = _deck[two];
-            _deck[two] = three;
+        return this._deck;
+    }
+    shuffle(inputDeck){
+        for(let i = 0; i < (Math.floor(Math.random()*10000)); i++){
+            let one = Math.floor(Math.random()*inputDeck.length);
+            let two = Math.floor(Math.random()*inputDeck.length);
+            let three = inputDeck[one];
+            inputDeck[one] = inputDeck[two];
+            inputDeck[two] = three;
         }
-    },
-    splitDeck(_deck){
-        let half = _deck.length / 2;
-        return firstHalf = _deck.splice(0,half), secondHalf = _deck.splice(half,_deck.length);        
+        return inputDeck;
+    }
+    splitDeck(){
+        let half = this._deck.length / 2;
+        return this.firstHalf = this._deck.splice(0,half), this.secondHalf = this._deck.splice(half,_deck.length);        
     }
 }
+let newDeck = new DECK();
+newDeck.getDeck();
+
+console.log(newDeck._deck);
+newDeck.shuffle(newDeck._deck);
+
+console.log(newDeck._deck);
+
+//DECK.shuffle(newDeck);
+//DECK.splitDeck(newDeck);
+//console.log(DECK.firstHalf);
 
 /*
  * Implementing the game of War using a tarot deck. This is an exercise  in manipulating the deck.
  */
 
 class Player {
-    constructor(){
-        this.winner = null;
-        this.points;
+    constructor(deck){
+        this.deck = deck;
     }
-    flipACard(pile){
-        pile.pop();
-    }
-    takeCards(){
-
+    flipCard(){
+        this.deck.shift();
     }
 }
-
-const playerOne = new Player;
-const playerTwo = new Player;
 
 class Game {
     constructor(){
-        this.gameState;
     }
     start(){
-        this.gameState = 1;
-        let deck = DECK.getDeck();
-        DECK.shuffle(deck);
-        DECK.splitDeck(deck);
-        this.turn();
-        if(this.gameState = 3){
+        this.deck = new DECK();
+        this.deck.shuffle();
+        this.deck.splitDeck(this.deck);
 
-        }
+        this.playerOne = new Player(this.deck.firstHalf);
+            console.log(this.playerOne);
+        this.playerTwo = new Player(this.deck.secondHalf);
+            console.log(this.playerTwo);
+        
+        this.turn();
     }
     turn(){
-        if(playerOne.pile  ){
-            
-        } else if (playerTwo.winner = true){
-            
-        }
+        while (this.playerOne.length != 0 || this.playerTwo.length != 0 ){
+            let flip1 = this.playerOne.flipCard();
+            let flip2 = this.playerTwo.flipCard();
+            if(flip1 > flip2){
+                playerOne.push(flip1, flip2);
+            } else if (flip2 > flip1){
+                playerTwo.push(flip1, flip2);
+            }
+    }
+        return;
     }
 }
 
+//let newGame = new Game();
+//console.log(newGame.start());
