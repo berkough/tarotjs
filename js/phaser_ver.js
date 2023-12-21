a936765291a98b551a713786b335cb18c80149c7
Mathew Kane <berkough@gmail.com>
2023-12-20 19:34:34 -0800

    first phaser lines working

--- js/phaser_ver.js
+++ js/phaser_ver.js
@@ -1,15 +1,12 @@
-import {Phaser} from '../node_modules/phaser/dist/phaser';
-import { TAROTDECK } from './tarot';
+import { TAROTDECK } from './tarot.js';
 
 class tableSpread extends Phaser.Scene {
     constructor(){
-        super();
+        super('tableSpread');
     }
     preload(){
         const deck = new TAROTDECK();
         deck.getDeck();
-        
-        console.log(deck);
 
         //Load the image for the "Deck" representation.
         this.load.image('cardback', '../cards/CardBack.jpg');
@@ -17,13 +14,13 @@ class tableSpread extends Phaser.Scene {
         //Load the Major Arcana images.
         for (let i = 0; i < 21; i++){
             let cardName = `${deck._deck[i].Place}-${deck._deck[i].Trump}`
-            this.load.image(cardName, cardName+'.jpg');
+            this.load.image(cardName, '../cards/'+cardName+'.jpg');
         }
 
         //Load the Minor Arcana images.
         for (let i = 22; i < 77; i++){
-            let cardName = `${deck._deck[i]}-${deck._deck[i].Place}`;
-            this.load.image(cardName, cardName+'.jpg');
+            let cardName = `${deck._deck[i].Place}-${deck._deck[i].Suit}`;
+            this.load.image(cardName, '../cards/'+cardName+'.jpg');
         }
 
     }
